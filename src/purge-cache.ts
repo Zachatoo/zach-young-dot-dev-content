6c443cbfa0eb288374ebeb4d83d8490fd081ca84
Zachatoo <Zachatoo@users.noreply.github.com>
2022-12-09 23:21:06 -0700

    Add purge cache script

---
+++ src/purge-cache.ts
@@ -0,0 +1,19 @@
+import { parse } from "https://deno.land/std@0.167.0/flags/mod.ts";
+
+const flags = parse(Deno.args, {
+  string: ["paths"],
+});
+
+const { paths } = flags;
+if (!paths) {
+  console.error("--paths is required");
+  Deno.exit();
+}
+
+const pathsArr: string[] = JSON.parse(paths);
+
+const urlsToPurge: string[] = [];
+pathsArr.forEach((path) => {
+  urlsToPurge.push(`https://zachyoung.dev/${path.replace(".md", "")}`);
+});
+console.log(urlsToPurge);
